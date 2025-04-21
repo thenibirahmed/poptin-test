c5acee2ab6db3d4040e49fac262705dd41a4a377
Nibir Ahmed <nibirahmed9611@gmail.com>
2025-04-21 12:45:51 +0600

    models bootstrapping

--- app/Models/PollOption.php
+++ app/Models/PollOption.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Poll;
 use Illuminate\Database\Eloquent\Model;
 
 class PollOption extends Model
 {
-    //
+    protected $fillable = [
+        'poll_id',
+        'option',
+    ];
+
+    public function poll()
+    {
+        return $this->belongsTo(Poll::class);
+    }
 }
--- app/Models/PollVote.php
+++ app/Models/PollVote.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\PollOption;
 use Illuminate\Database\Eloquent\Model;
 
 class PollVote extends Model
 {
-    //
+    protected $fillable = [
+        'poll_option_id',
+        'user_id',
+        'ip_address',
+    ];
+
+    public function pollOption()
+    {
+        return $this->belongsTo(PollOption::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
